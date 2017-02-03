deafc7e9aa7a9667948827b25242dda7757aa74c
manish.ojha <manish.ojha@INBLRL5402.corp.ad.cginet>
2017-02-03 19:17:00 +0530

    demo4j

---
+++ src/qsp/Demo.java
@@ -0,0 +1,25 @@
+package qsp;
+
+import org.apache.log4j.Logger;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		Logger l=Logger.getLogger("Demo");
+		l.fatal("Fatal1");
+		l.error("error1");
+		l.warn("warn1");
+		l.info("info1");
+		l.debug("debug1");
+		l.trace("trace1");
+		
+		l.fatal("Fatal2");
+		l.error("error2");
+		l.warn("warn2");
+		l.info("info2");
+		l.debug("debug2");
+		l.trace("trace2");
+	}
+
+}
